0c502e92fb7c79e5f38339b4a20d76e48806aa8a
Gejun Zhu <gejun.anderson@gmail.com>
2021-08-18 11:27:11 -0500

    add boundaries cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,6 +28,9 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info('Change last_review to DateTime')
     df['last_review'] = pd.to_datetime(df['last_review'])
 
